623691576e29c47852c5755f120eddd8a791bc77
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-03 16:58:00 +0300

    09.Практика по import/export

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import './App.css';
+import Technologies from './Technologies';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Technologies />
-    </div>
-  );
-}
-
-const Technologies = () => {
-  return (
-    <div>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JS</li>
-          <li>React</li>
-        </ul>
-    </div>
-  );
-}
-
-const Header = () => {
-  return (
-    <div className="App">
-      <a href="#">HTML</a>
-      <a href="#">CSS</a>
-      <a href="#">JS</a>
-      <a href="#">React</a>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+      <div className="App">
+        <a href="#">HTML</a>
+        <a href="#">CSS</a>
+        <a href="#">JS</a>
+        <a href="#">React</a>
+      </div>
+    );
+}
+
+export default Header;
