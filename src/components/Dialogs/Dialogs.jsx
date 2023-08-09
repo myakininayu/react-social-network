0a50b7e0cbfff90b3a8a0146eca0941ea9a00303
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-09 18:00:34 +0300

    21.Верстка страницы Диалогов

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,29 @@ import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return(
-    <div>
-        Dialogs
+    <div className={style.dialogs}>
+        <div className={style.dialogItems}>
+            <div className={style.dialog + ' ' +style.active}>
+                Yulia
+            </div>
+            <div className={style.dialog}>
+                Valeria
+            </div>
+            <div className={style.dialog}>
+                Ekaterina
+            </div>
+        </div>
+        <div className={style.messageItems}>
+            <div className={style.message}>
+                Hii!
+            </div>
+            <div className={style.message}>
+                How are you?
+            </div>
+            <div className={style.message}>
+                Yo
+            </div>
+        </div>
     </div>
     );
 }
