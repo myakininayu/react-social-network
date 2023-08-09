932d02eb3201bfc60ba68bdd375cc8dc0dbd9808
Myakinina Yulia <myakinina_yu@mail.ru>
2023-08-09 18:23:38 +0300

    22.Exact routing for Dialogs component

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,31 +1,32 @@
 import style from './Dialogs.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Dialogs = () => {
-    return(
-    <div className={style.dialogs}>
-        <div className={style.dialogItems}>
-            <div className={style.dialog + ' ' +style.active}>
-                Yulia
+    return (
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + ' ' + style.active}>
+                    <NavLink to='/dialogs/1'>Yulia</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/2'>Valeria</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/3'>Ekaterina</NavLink>
+                </div>
             </div>
-            <div className={style.dialog}>
-                Valeria
-            </div>
-            <div className={style.dialog}>
-                Ekaterina
+            <div className={style.messageItems}>
+                <div className={style.message}>
+                    Hii!
+                </div>
+                <div className={style.message}>
+                    How are you?
+                </div>
+                <div className={style.message}>
+                    Yo
+                </div>
             </div>
         </div>
-        <div className={style.messageItems}>
-            <div className={style.message}>
-                Hii!
-            </div>
-            <div className={style.message}>
-                How are you?
-            </div>
-            <div className={style.message}>
-                Yo
-            </div>
-        </div>
-    </div>
     );
 }
 
